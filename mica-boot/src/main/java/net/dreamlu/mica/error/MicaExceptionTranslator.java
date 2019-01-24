eec4fb5aec2b07c56e89221650382559a2e9a087
如梦技术 <596392912@qq.com>
2019-01-24 12:32:00 +0800

    :tada: 0.0.1-RC1

---
+++ mica-boot/src/main/java/net/dreamlu/mica/error/MicaExceptionTranslator.java
@@ -0,0 +1,80 @@
+package net.dreamlu.mica.error;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.dreamlu.mica.core.exception.ServiceException;
+import net.dreamlu.mica.core.result.R;
+import net.dreamlu.mica.core.result.SystemCode;
+import net.dreamlu.mica.core.utils.Exceptions;
+import net.dreamlu.mica.core.utils.ObjectUtil;
+import net.dreamlu.mica.core.utils.WebUtil;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.Servlet;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * mica 未知异常转译和发送，方便监听，对未知异常统一处理。Order 排序优先级低
+ *
+ * @author L.cm
+ */
+@Slf4j
+@Order
+@Configuration
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
+@ConditionalOnClass({ Servlet.class, DispatcherServlet.class })
+@RestControllerAdvice
+@AllArgsConstructor
+public class MicaExceptionTranslator {
+	private final ApplicationEventPublisher publisher;
+
+	@ExceptionHandler(ServiceException.class)
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	public R handleError(ServiceException e) {
+		log.error("业务异常", e);
+		R result = e.getResult();
+		if (result == null) {
+			// 发送：未知业务异常事件
+			result = R.fail(SystemCode.FAILURE, e.getMessage());
+			publishEvent(e);
+		}
+		return result;
+	}
+
+	@ExceptionHandler(Throwable.class)
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	public R handleError(Throwable e) {
+		log.error("未知异常", e);
+		// 发送：未知异常异常事件
+		publishEvent(e);
+		return R.fail(SystemCode.FAILURE);
+	}
+
+	private void publishEvent(Throwable error) {
+		HttpServletRequest request = WebUtil.getRequest();
+		MicaErrorEvent event = new MicaErrorEvent();
+		event.setRequestUrl(request.getRequestURI());
+		event.setStackTrace(Exceptions.getStackTraceAsString(error));
+		event.setExceptionName(error.getClass().getName());
+		event.setMessage(error.getMessage());
+		StackTraceElement[] elements = error.getStackTrace();
+		if (ObjectUtil.isNotEmpty(elements)) {
+			StackTraceElement element = elements[0];
+			event.setClassName(element.getClassName());
+			event.setFileName(element.getFileName());
+			event.setMethodName(element.getMethodName());
+			event.setLineNumber(element.getLineNumber());
+		}
+		// 发布事件
+		publisher.publishEvent(event);
+	}
+}
--- mica-boot/src/main/java/net/dreamlu/mica/error/RestExceptionTranslator.java
+++ mica-boot/src/main/java/net/dreamlu/mica/error/RestExceptionTranslator.java
@@ -16,20 +16,16 @@
 
 package net.dreamlu.mica.error;
 
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import net.dreamlu.mica.core.exception.ServiceException;
 import net.dreamlu.mica.core.result.R;
 import net.dreamlu.mica.core.result.SystemCode;
-import net.dreamlu.mica.core.utils.Exceptions;
-import net.dreamlu.mica.core.utils.ObjectUtil;
 import net.dreamlu.mica.core.utils.StringUtil;
-import net.dreamlu.mica.core.utils.WebUtil;
 import org.hibernate.validator.internal.engine.path.PathImpl;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindException;
@@ -48,13 +44,12 @@ import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
 import javax.servlet.Servlet;
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 import java.util.Set;
 
 /**
- * 全局异常处理，处理可预见的异常
+ * 全局异常处理，处理可预见的异常，Order 排序优先级高
  *
  * <p>
  *     Bean-Violation 异常处理
@@ -63,13 +58,12 @@ import java.util.Set;
  * @author L.cm
  */
 @Slf4j
+@Order(Ordered.HIGHEST_PRECEDENCE)
 @Configuration
 @ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
 @ConditionalOnClass({ Servlet.class, DispatcherServlet.class })
 @RestControllerAdvice
-@AllArgsConstructor
 public class RestExceptionTranslator {
-	private final ApplicationEventPublisher eventPublisher;
 
 	@ExceptionHandler(MissingServletRequestParameterException.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
@@ -154,44 +148,4 @@ public class RestExceptionTranslator {
 		return R.fail(SystemCode.MEDIA_TYPE_NOT_SUPPORTED, message);
 	}
 
-	@ExceptionHandler(ServiceException.class)
-	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-	public R handleError(ServiceException e) {
-		log.error("业务异常", e);
-		R result = e.getResult();
-		if (result == null) {
-			// 发送：未知业务异常事件
-			result = R.fail(SystemCode.FAILURE, e.getMessage());
-			publishEvent(e);
-		}
-		return result;
-	}
-
-	@ExceptionHandler(Throwable.class)
-	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-	public R handleError(Throwable e) {
-		log.error("未知异常", e);
-		// 发送：未知异常异常事件
-		publishEvent(e);
-		return R.fail(SystemCode.FAILURE);
-	}
-
-	private void publishEvent(Throwable error) {
-		HttpServletRequest request = WebUtil.getRequest();
-		MicaErrorEvent event = new MicaErrorEvent();
-		event.setRequestUrl(request.getRequestURI());
-		event.setStackTrace(Exceptions.getStackTraceAsString(error));
-		event.setExceptionName(error.getClass().getName());
-		event.setMessage(error.getMessage());
-		StackTraceElement[] elements = error.getStackTrace();
-		if (ObjectUtil.isNotEmpty(elements)) {
-			StackTraceElement element = elements[0];
-			event.setClassName(element.getClassName());
-			event.setFileName(element.getFileName());
-			event.setMethodName(element.getMethodName());
-			event.setLineNumber(element.getLineNumber());
-		}
-		// 发布事件
-		eventPublisher.publishEvent(event);
-	}
 }
