5d1174bc202f65f5efe6528ad05f4c80827dd21b
如梦技术 <596392912@qq.com>
2019-07-26 21:00:05 +0800

    :zap: mica-http 添加重试机制.

--- mica-http/src/main/java/net/dreamlu/mica/http/RetryInterceptor.java
+++ mica-http/src/main/java/net/dreamlu/mica/http/RetryInterceptor.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2019-2029, Dreamlu (596392912@qq.com & www.dreamlu.net).
+ * <p>
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE 3.0;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.gnu.org/licenses/lgpl.html
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.dreamlu.mica.http;
 
 import net.dreamlu.mica.core.utils.ThreadUtil;
@@ -11,7 +27,7 @@ import java.util.concurrent.Callable;
 /**
  * 重试拦截器，应对代理问题
  *
- * @author dream.lu
+ * @author L.cm
  */
 public class RetryInterceptor implements Interceptor {
 	private final RetryPolicy retryPolicy;
