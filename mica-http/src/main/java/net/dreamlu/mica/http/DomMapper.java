6fc21e519a9c6808fbb5eeea25fda5b2f56183c1
如梦技术 <596392912@qq.com>
2019-08-05 21:33:58 +0800

    :zap: 优化 DomMapper 添加更多方法.

--- mica-http/src/main/java/net/dreamlu/mica/http/DomMapper.java
+++ mica-http/src/main/java/net/dreamlu/mica/http/DomMapper.java
@@ -16,10 +16,16 @@
 
 package net.dreamlu.mica.http;
 
+import org.jsoup.helper.DataUtil;
+import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.parser.Parser;
 import org.jsoup.select.Elements;
 import org.springframework.cglib.proxy.Enhancer;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,6 +36,36 @@ import java.util.List;
  */
 @SuppressWarnings("unchecked")
 public class DomMapper {
+
+	/**
+	 * 读取 xml 信息为 java Bean
+	 *
+	 * @param inputStream InputStream
+	 * @param clazz       bean Class
+	 * @param <T>         泛型
+	 * @return 对象
+	 */
+	public static <T> T readValue(InputStream inputStream, final Class<T> clazz) {
+		try {
+			Document document = DataUtil.load(inputStream, StandardCharsets.UTF_8.name(), "");
+			return readValue(document, clazz);
+		} catch (IOException e) {
+			throw new MicaHttpException(e);
+		}
+	}
+
+	/**
+	 * 读取 xml 信息为 java Bean
+	 *
+	 * @param html  html String
+	 * @param clazz bean Class
+	 * @param <T>   泛型
+	 * @return 对象
+	 */
+	public static <T> T readValue(String html, final Class<T> clazz) {
+		Document document = Parser.parse(html, "");
+		return readValue(document, clazz);
+	}
 
 	/**
 	 * 读取 xml 信息为 java Bean
@@ -45,6 +81,36 @@ public class DomMapper {
 		enhancer.setUseCache(true);
 		enhancer.setCallback(new CssQueryMethodInterceptor(clazz, doc));
 		return (T) enhancer.create();
+	}
+
+	/**
+	 * 读取 xml 信息为 java Bean
+	 *
+	 * @param <T>         泛型
+	 * @param inputStream InputStream
+	 * @param clazz       bean Class
+	 * @return 对象
+	 */
+	public static <T> List<T> readList(InputStream inputStream, final Class<T> clazz) {
+		try {
+			Document document = DataUtil.load(inputStream, StandardCharsets.UTF_8.name(), "");
+			return readList(document, clazz);
+		} catch (IOException e) {
+			throw new MicaHttpException(e);
+		}
+	}
+
+	/**
+	 * 读取 xml 信息为 java Bean
+	 *
+	 * @param <T>   泛型
+	 * @param html  html String
+	 * @param clazz bean Class
+	 * @return 对象
+	 */
+	public static <T> List<T> readList(String html, final Class<T> clazz) {
+		Document document = Parser.parse(html, "");
+		return readList(document, clazz);
 	}
 
 	/**
