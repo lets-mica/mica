f9abfd6e7d8ecb7e49604c614f8fe611dc50de4d
如梦技术 <596392912@qq.com>
2019-09-02 12:20:20 +0800

    :zap: 优化 Pair.

--- mica-core/src/main/java/net/dreamlu/mica/core/tuple/Pair.java
+++ mica-core/src/main/java/net/dreamlu/mica/core/tuple/Pair.java
@@ -26,8 +26,59 @@ import lombok.*;
 @Getter
 @ToString
 @EqualsAndHashCode
-@AllArgsConstructor
-public class Pair<T, R> {
-	private final T first;
-	private final R last;
+public class Pair<L, R> {
+	private static final Pair<Object, Object> EMPTY = new Pair<>(null, null);
+
+	private final L left;
+	private final R right;
+
+	/**
+	 * Returns an empty pair.
+	 */
+	@SuppressWarnings("unchecked")
+	public static <L, R> Pair<L, R> empty() {
+		return (Pair<L, R>) EMPTY;
+	}
+
+	/**
+	 * Constructs a pair with its left value being {@code left}, or returns an empty pair if
+	 * {@code left} is null.
+	 *
+	 * @return the constructed pair or an empty pair if {@code left} is null.
+	 */
+	public static <L, R> Pair<L, R> createLeft(L left) {
+		if (left == null) {
+			return empty();
+		} else {
+			return new Pair<>(left, null);
+		}
+	}
+
+	/**
+	 * Constructs a pair with its right value being {@code right}, or returns an empty pair if
+	 * {@code right} is null.
+	 *
+	 * @return the constructed pair or an empty pair if {@code right} is null.
+	 */
+	public static <L, R> Pair<L, R> createRight(R right) {
+		if (right == null) {
+			return empty();
+		} else {
+			return new Pair<>(null, right);
+		}
+	}
+
+	public static <L, R> Pair<L, R> create(L left, R right) {
+		if (right == null && left == null) {
+			return empty();
+		} else {
+			return new Pair<>(left, right);
+		}
+	}
+
+	private Pair(L left, R right) {
+		this.left = left;
+		this.right = right;
+	}
+
 }
