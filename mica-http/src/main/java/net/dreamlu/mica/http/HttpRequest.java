e749623a0199e4214864f5fda31da2cdc5e37f84
如梦技术 <596392912@qq.com>
2019-09-10 17:40:22 +0800

    :zap: 添加基于 CompletableFuture 的 executeAsync.

--- mica-http/src/main/java/net/dreamlu/mica/http/HttpRequest.java
+++ mica-http/src/main/java/net/dreamlu/mica/http/HttpRequest.java
@@ -40,6 +40,8 @@ import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Predicate;
 
@@ -281,6 +283,14 @@ public class HttpRequest {
 		} catch (IOException e) {
 			return new ErrorResponse(call.request(), e);
 		}
+	}
+
+	public ResponseSpec executeAsync() {
+		return CompletableFuture.supplyAsync(this::execute).join();
+	}
+
+	public ResponseSpec executeAsync(Executor executor) {
+		return CompletableFuture.supplyAsync(this::execute, executor).join();
 	}
 
 	public AsyncCall async() {
