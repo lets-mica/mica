a97db1b4cfb4b8e84b26b0b38b4b96718a656ced
如梦技术 <596392912@qq.com>
2019-09-25 21:39:57 +0800

    :tada: 添加 mica-actuator 模块，为 mica-admin 做铺垫。

---
+++ mica-actuator/README.md
@@ -0,0 +1,20 @@
+# mica-actuator
+用于扩展更多端点，便于在 admin 中进行监控。
+
+## 功能
+1. druid star 端点。
+
+## 添加依赖
+### maven
+```xml
+<dependency>
+  <groupId>net.dreamlu</groupId>
+  <artifactId>mica-actuator</artifactId>
+  <version>${version}</version>
+</dependency>
+```
+
+### gradle
+```groovy
+compile("net.dreamlu:mica-actuator:${version}")
+```
---
+++ mica-actuator/src/main/java/net/dreamlu/mica/actuator/druid/DruidWebEndpoint.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2019-2029, Dreamlu 卢春梦 (596392912@qq.com & www.dreamlu.net).
+ * <p>
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE 3.0;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.gnu.org/licenses/lgpl.html
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.dreamlu.mica.actuator.druid;
+
+import com.alibaba.druid.stat.JdbcStatManager;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.boot.actuate.endpoint.web.annotation.WebEndpoint;
+
+import javax.management.JMException;
+import javax.management.openmbean.TabularData;
+
+/**
+ * druid stat 端点
+ *
+ * @author L.cm
+ */
+@WebEndpoint(id = "druid")
+public class DruidWebEndpoint {
+
+	@ReadOperation
+	public TabularData health() throws JMException {
+		JdbcStatManager instance = JdbcStatManager.getInstance();
+		return instance.getSqlList();
+	}
+
+}
