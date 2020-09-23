7553f104af3260227f1ebc226f54929cf5654de5
如梦技术 <596392912@qq.com>
2020-09-23 23:17:26 +0800

    :sparkles: 优化 mica-jobs 模块。

--- mica-jobs/src/main/java/net/dreamlu/mica/jobs/config/XxlJobClientAutoConfiguration.java
+++ mica-jobs/src/main/java/net/dreamlu/mica/jobs/config/XxlJobClientAutoConfiguration.java
@@ -44,13 +44,13 @@ import static net.dreamlu.mica.jobs.properties.XxlJobClientProperties.XxlJobExec
  * @author L.cm
  */
 @Slf4j
-@Configuration
+@Configuration(proxyBeanMethods = false)
 @EnableConfigurationProperties(XxlJobClientProperties.class)
 public class XxlJobClientAutoConfiguration {
 	private static final String LB_PREFIX = "lb://";
 
 	@Bean
-	@ConditionalOnProperty(prefix = "xxl.job", value = "enabled", havingValue = "true")
+	@ConditionalOnProperty(prefix = "xxl.job", value = "enabled", havingValue = "true", matchIfMissing = true)
 	public XxlJobSpringExecutor xxlJobExecutor(XxlJobClientProperties properties,
 											   DiscoveryClient discoveryClient,
 											   Environment environment,
