bb338dc3228d1fe192206186375d0a2dd9593095
如梦技术 <596392912@qq.com>
2020-10-08 22:19:24 +0800

    :sparkles: mica-core 抽取 Pkcs7Encoder。

--- mica-core/src/main/java/net/dreamlu/mica/core/utils/AesUtil.java
+++ mica-core/src/main/java/net/dreamlu/mica/core/utils/AesUtil.java
@@ -126,41 +126,4 @@ public class AesUtil {
 		}
 	}
 
-	/**
-	 * 提供基于PKCS7算法的加解密接口.
-	 */
-	private static class Pkcs7Encoder {
-		private static int BLOCK_SIZE = 32;
-
-		private static byte[] encode(byte[] src) {
-			int count = src.length;
-			// 计算需要填充的位数
-			int amountToPad = BLOCK_SIZE - (count % BLOCK_SIZE);
-			if (amountToPad == 0) {
-				amountToPad = BLOCK_SIZE;
-			}
-			// 获得补位所用的字符
-			byte pad = (byte) (amountToPad & 0xFF);
-			byte[] pads = new byte[amountToPad];
-			for (int index = 0; index < amountToPad; index++) {
-				pads[index] = pad;
-			}
-			int length = count + amountToPad;
-			byte[] dest = new byte[length];
-			System.arraycopy(src, 0, dest, 0, count);
-			System.arraycopy(pads, 0, dest, count, amountToPad);
-			return dest;
-		}
-
-		private static byte[] decode(byte[] decrypted) {
-			int pad = decrypted[decrypted.length - 1];
-			if (pad < 1 || pad > BLOCK_SIZE) {
-				pad = 0;
-			}
-			if (pad > 0) {
-				return Arrays.copyOfRange(decrypted, 0, decrypted.length - pad);
-			}
-			return decrypted;
-		}
-	}
 }
