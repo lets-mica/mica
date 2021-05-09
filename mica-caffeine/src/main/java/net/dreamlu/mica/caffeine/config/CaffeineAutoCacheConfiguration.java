15105fcbfd7edc7ef7632f4daae54dcae82b132c
如梦技术 <596392912@qq.com>
2021-05-09 15:44:12 +0800

    :sparkles: 完善 spring-native 支持 mica-caffeine 可以启动 #38 #I3PX2N

--- mica-caffeine/src/main/java/net/dreamlu/mica/caffeine/config/CaffeineAutoCacheConfiguration.java
+++ mica-caffeine/src/main/java/net/dreamlu/mica/caffeine/config/CaffeineAutoCacheConfiguration.java
@@ -55,10 +55,43 @@ import java.util.stream.Collectors;
 @ConditionalOnClass({Caffeine.class, CaffeineCacheManager.class})
 @AutoConfigureBefore(name = "org.springframework.boot.autoconfigure.cache.CaffeineCacheConfiguration")
 @NativeHint(types = {
-		@TypeHint(types = CaffeineAutoCacheManager.class, access = AccessBits.ALL),
-		@TypeHint(types = CaffeineCacheManager.class, access = AccessBits.ALL),
-        @TypeHint(typeNames = "com.github.benmanes.caffeine.cache.SSA", access = AccessBits.ALL),
-        @TypeHint(typeNames = "com.github.benmanes.caffeine.cache.UnsafeAccess", fields = @FieldHint(name = "UNSAFE", allowUnsafeAccess = true), access = AccessBits.PUBLIC_METHODS)
+	@TypeHint(types = CaffeineAutoCacheManager.class, access = AccessBits.ALL),
+	@TypeHint(types = CaffeineCacheManager.class, access = AccessBits.ALL),
+	@TypeHint(typeNames = "com.github.benmanes.caffeine.cache.UnsafeAccess",
+		fields = @FieldHint(name = "UNSAFE", allowUnsafeAccess = true),
+		access = AccessBits.PUBLIC_METHODS
+	),
+	@TypeHint(types = Thread.class, access = AccessBits.DECLARED_FIELDS),
+	@TypeHint(typeNames = "com.github.benmanes.caffeine.cache.PS",
+		fields = {
+			@FieldHint(name = "key", allowUnsafeAccess = true),
+			@FieldHint(name = "value", allowUnsafeAccess = true)
+		},
+		access = AccessBits.DECLARED_CONSTRUCTORS
+	),
+	@TypeHint(typeNames = "com.github.benmanes.caffeine.cache.PSA",
+		fields = @FieldHint(name = "accessTime", allowUnsafeAccess = true),
+		access = AccessBits.DECLARED_CONSTRUCTORS
+	),
+	@TypeHint(typeNames = "com.github.benmanes.caffeine.cache.PSW",
+		fields = @FieldHint(name = "writeTime", allowUnsafeAccess = true),
+		access = AccessBits.DECLARED_CONSTRUCTORS
+	),
+	@TypeHint(typeNames = "com.github.benmanes.caffeine.cache.StripedBuffer",
+		fields = {@FieldHint(name = "tableBusy", allowUnsafeAccess = true)},
+		access = AccessBits.ALL
+	),
+	@TypeHint(typeNames = "com.github.benmanes.caffeine.cache.PSWMS", access = AccessBits.DECLARED_CONSTRUCTORS),
+	@TypeHint(typeNames = "com.github.benmanes.caffeine.cache.SSA", access = AccessBits.ALL),
+	@TypeHint(typeNames = "com.github.benmanes.caffeine.cache.SSLA", access = AccessBits.DECLARED_CONSTRUCTORS),
+	@TypeHint(typeNames = "com.github.benmanes.caffeine.cache.SSLMSW", access = AccessBits.DECLARED_CONSTRUCTORS),
+	@TypeHint(typeNames = "com.github.benmanes.caffeine.cache.SSMSW", access = AccessBits.DECLARED_CONSTRUCTORS),
+	@TypeHint(typeNames = "com.github.benmanes.caffeine.cache.BoundedBuffer", access = AccessBits.ALL),
+	@TypeHint(typeNames = "com.github.benmanes.caffeine.cache.BoundedBuffer$RingBuffer", access = AccessBits.ALL),
+	@TypeHint(typeNames = "com.github.benmanes.caffeine.cache.BLCHeader$DrainStatusRef",
+		fields = @FieldHint(name = "drainStatus", allowUnsafeAccess = true),
+		access = AccessBits.ALL
+	)
 })
 public class CaffeineAutoCacheConfiguration {
 
