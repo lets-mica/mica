546d8dd8a5d626e64ec0e7be84f282e87f147dca
如梦技术 <596392912@qq.com>
2021-09-29 16:42:58 +0800

    :sparkles: BeanTest

--- mica-core/src/test/java/net/dreamlu/mica/test/bean/BeanTest.java
+++ mica-core/src/test/java/net/dreamlu/mica/test/bean/BeanTest.java
@@ -5,23 +5,26 @@ import net.dreamlu.mica.test.utils.BeanCopyUtilTest;
 import org.springframework.cglib.beans.BeanCopier;
 import org.springframework.cglib.core.Converter;
 import org.springframework.cglib.core.DebuggingClassWriter;
+import org.springframework.cglib.core.Predicate;
+import org.springframework.cglib.core.SpringNamingPolicy;
 
 public class BeanTest {
 
 	@Data
-	private static class User {
+	public static class User {
 		private Integer id;
 		private String name;
 		private Integer age;
 	}
 
 	@Data
-	private static class UserVO {
+	public static class UserVO {
 		private String name;
 		private Integer age;
 	}
 
-	public static void test1() {
+	public static void test0() {
+		// 注意：升级到 java17 之后这样是有问题的，需要采用 test1 的方式
 		BeanCopier beanCopier = BeanCopier.create(User.class, UserVO.class, false);
 		User user = new User();
 		user.setId(1);
@@ -33,7 +36,34 @@ public class BeanTest {
 		System.out.println(userVO);
 	}
 
+	public static void test1() {
+		BeanCopier.Generator gen = new BeanCopier.Generator();
+		gen.setSource(User.class);
+		gen.setTarget(UserVO.class);
+		gen.setUseConverter(false);
+		gen.setContextClass(BeanCopier.class);
+		gen.setNamingPolicy(new SpringNamingPolicy() {
+			@Override
+			public String getClassName(String prefix, String source, Object key, Predicate names) {
+				// 保证 生成的 copy 类和 ContextClass 在一个包
+				return super.getClassName(BeanCopier.class.getName(), source, key, names);
+			}
+		});
+		gen.setClassLoader(UserVO.class.getClassLoader());
+		BeanCopier beanCopier = gen.create();
+
+		User user = new User();
+		user.setId(1);
+		user.setName("如梦技术");
+		user.setAge(18);
+
+		UserVO userVO = new UserVO();
+		beanCopier.copy(user, userVO, null);
+		System.out.println(userVO);
+	}
+
 	public static void test2() {
+		// 升级到 java 17 之后 User、UserVO 不能为 private 的内部内，否者会报错：in unnamed module of loader 'app'
 		BeanCopier beanCopier = BeanCopier.create(User.class, UserVO.class, true);
 		User user = new User();
 		user.setId(1);
@@ -56,6 +86,7 @@ public class BeanTest {
 		String sourcePath = BeanCopyUtilTest.class.getResource("/").getPath().split("mica-core")[0];
 		System.setProperty(DebuggingClassWriter.DEBUG_LOCATION_PROPERTY, sourcePath + "gen_code");
 
+//		test0();
 		test1();
 		test2();
 	}
