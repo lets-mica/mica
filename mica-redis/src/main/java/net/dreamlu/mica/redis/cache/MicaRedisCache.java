f6d9653f44098b0d4a71fda4586666a7ac5a9619
如梦技术 <596392912@qq.com>
2021-11-22 20:07:17 +0800

    :sparkles: mica-redis 添加 sCard 方法。

--- mica-redis/src/main/java/net/dreamlu/mica/redis/cache/MicaRedisCache.java
+++ mica-redis/src/main/java/net/dreamlu/mica/redis/cache/MicaRedisCache.java
@@ -461,6 +461,16 @@ public class MicaRedisCache {
 	}
 
 	/**
+	 * 返回集合中元素的数量。
+	 * @param key redis key
+	 * @return 数量
+	 */
+	@Nullable
+	public Long sCard(String key) {
+		return setOps.size(key);
+	}
+
+	/**
 	 * 同时设置一个或多个 key-value 对。
 	 * 如果某个给定 key 已经存在，那么 MSET 会用新值覆盖原来的旧值，如果这不是你所希望的效果，请考虑使用 MSETNX 命令：它只会在所有给定 key 都不存在的情况下进行设置操作。
 	 * MSET 是一个原子性(atomic)操作，所有给定 key 都会在同一时间内被设置，某些给定 key 被更新而另一些给定 key 没有改变的情况，不可能发生。
