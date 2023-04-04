2205e59dd777ecd0b54d7d1e0cf30d5bdfd5c7ac
浅梦 <1101766085@qq.com>
2023-04-04 10:30:19 +0800

    :sparkles: mica-http 优化

--- mica-http/src/test/java/net/dreamlu/HttpTest.java
+++ mica-http/src/test/java/net/dreamlu/HttpTest.java
@@ -2,20 +2,19 @@ package net.dreamlu;
 
 import net.dreamlu.mica.http.HttpRequest;
 import net.dreamlu.mica.http.LogLevel;
-import net.dreamlu.mica.http.ResponseSpec;
 
 public class HttpTest {
 
 	public static void main(String[] args) {
-		String html = HttpRequest.get("https://wwww.baiduxxx.com/123123")
+		String html = HttpRequest.get("https://www.dreamluxxx.net/123123")
 			.useSlf4jLog(LogLevel.NONE)
 			.execute()
-			.onFailed((request, e) -> {
+			.onResponse(responseSpec -> {
+				return responseSpec.asString();
+			}, (request, e) -> {
 				e.printStackTrace();
-				ResponseSpec response = e.getResponse();
-				System.out.println(response.asString());
-			})
-			.onSuccessful(ResponseSpec::asString);
+				return null;
+			});
 		System.out.println(html);
 	}
 
