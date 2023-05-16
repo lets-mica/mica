c8a69da20759f7e76b4e6a8aeffc813f08de4d0a
浅梦 <1101766085@qq.com>
2023-05-16 14:06:47 +0800

    :bug: 修复 form62Str 转回不准的问题

--- mica-core/src/test/java/net/dreamlu/mica/test/utils/NumberTest.java
+++ mica-core/src/test/java/net/dreamlu/mica/test/utils/NumberTest.java
@@ -7,12 +7,22 @@ import org.junit.jupiter.api.Test;
 class NumberTest {
 
 	@Test
-	void testTo62String() {
+	void testTo62String1() {
 		long ms = 1551320493447L;
 		String string = NumberUtil.to62Str(ms);
 		Assertions.assertEquals("rjkOH7p", string);
 		long l = NumberUtil.form62Str(string);
 		Assertions.assertEquals(ms, l);
 	}
+
+	@Test
+	void testTo62String2() {
+		long ms = 1657930102030434306L;
+		String string = NumberUtil.to62Str(ms);
+		Assertions.assertEquals("1YtkVbn657s", string);
+		long l = NumberUtil.form62Str(string);
+		Assertions.assertEquals(ms, l);
+	}
+
 
 }
