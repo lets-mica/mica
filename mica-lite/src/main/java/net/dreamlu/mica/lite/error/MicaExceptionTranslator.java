3bf219f227a3cfc2dd6d2b6dfa13ff6c6537a252
m2 <m2@qq.com>
2024-04-09 10:48:19 +0800

    refactor(MicaExceptionTranslator): change log position

--- mica-lite/src/main/java/net/dreamlu/mica/lite/error/MicaExceptionTranslator.java
+++ mica-lite/src/main/java/net/dreamlu/mica/lite/error/MicaExceptionTranslator.java
@@ -49,9 +49,9 @@ public class MicaExceptionTranslator {
 	@ExceptionHandler(ServiceException.class)
 	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 	public R<Object> handleError(ServiceException e) {
-		log.error("业务异常", e);
 		R<Object> result = e.getResult();
 		if (result == null) {
+			log.error("业务异常", e);
 			// 发送：未知业务异常事件
 			result = R.fail(SystemCode.FAILURE, e.getMessage());
 			publishEvent(e);
