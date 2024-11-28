5276368e0ff2c004b6ce9e8e4adbd025b9c19c00
浅梦 <1101766085@qq.com>
2024-11-28 15:44:11 +0800

    :sparkles: mica-activerecord 优化，只有在开启 自动表扫描 时校验 模型的包路径

--- mica-activerecord/src/main/java/net/dreamlu/mica/activerecord/config/ActiveRecordConfiguration.java
+++ mica-activerecord/src/main/java/net/dreamlu/mica/activerecord/config/ActiveRecordConfiguration.java
@@ -146,8 +146,6 @@ public class ActiveRecordConfiguration {
 												 ResourceLoader resourceLoader,
 												 MicaActiveRecordProperties properties,
 												 ObjectProvider<ActiveRecordPluginCustomizer> customizerObjectProvider) {
-		String modelPackage = properties.getModelPackage();
-		Assert.hasText(modelPackage, "mica.activerecord.model-package is blank.");
 		ActiveRecordPlugin arp = new ActiveRecordPlugin(dataSourceProvider);
 		arp.setDialect(properties.getDialect().getDialect());
 		arp.setTransactionLevel(properties.getTransactionLevel().getLevel());
@@ -162,6 +160,8 @@ public class ActiveRecordConfiguration {
 		}
 		// 扫描和添加表映射
 		if (properties.isAutoTableScan()) {
+			String modelPackage = properties.getModelPackage();
+			Assert.hasText(modelPackage, "mica.activerecord.model-package is blank.");
 			scanTable(arp, environment, resourceLoader, modelPackage);
 		}
 		// arp 自定义配置 bean
