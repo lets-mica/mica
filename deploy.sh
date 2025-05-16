58bfda6904e4923b13d855364b9df5b246cb4318
如梦技术 <596392912@qq.com>
2025-05-16 17:28:42 +0800

    :hammer: 使用 github action 发布快照版

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,13 @@ printf "\n"
 mvn -version
 printf "\n"
 
-## 3. deploy 发布正式版，
-mvn clean package deploy -Prelease -DskipTests
+## 3. 环境
+if [ -z $1 ]; then
+    profile="release"
+else
+    profile="$1"
+fi
+
+## 4. deploy 发布正式版，
+mvn clean package deploy -P$profile -DskipTests
 
